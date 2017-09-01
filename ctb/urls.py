32c4b44d58251e0f40e13bfc77378cf89904dadf
Eduardo Lucas <eduardolucas40@gmail.com>
2017-09-01 13:20:08 -0300

    6th commit - Delete with confirmation

--- ctb/urls.py
+++ ctb/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
 
 from ctb import views
-from ctb.views import HistoricoList, HistoricoDetalhe, HistoricoCreate, HistoricoUpdate, HistoricoDelete, ContaList, \
-    ContaDetalhe, ContaCreate, ContaUpdate,  ContaDelete
+from ctb.views import HistoricoList, HistoricoDetalhe, HistoricoCreate, HistoricoUpdate, historico_delete, \
+    ContaList, ContaDetalhe, ContaCreate, ContaUpdate, conta_delete
 
 app_name = 'ctb'
 
@@ -12,14 +12,14 @@ urlpatterns = [
     url(r'^conta-list/(?P<pk>[0-9]+)/$', ContaDetalhe.as_view(), name="conta-detail"),
     url(r'^conta-add/$', ContaCreate.as_view(), name='conta-add'),
     url(r'^conta/(?P<pk>[0-9]+)/edit/$', ContaUpdate.as_view(), name='conta-edit'),
-    url(r'^conta/(?P<pk>[0-9]+)/delete/$', ContaDelete.as_view(), name='conta-delete'),
+    url(r'^conta/(?P<id>[0-9]+)/delete/$', conta_delete, name='conta-delete'),
 
     # Historicos
     url(r'^historico-list/$', HistoricoList.as_view(), name='historico-list'),
     url(r'^historico-list/(?P<pk>[0-9]+)/$', HistoricoDetalhe.as_view(), name='historico-detail'),
     url(r'^historico-add/$', HistoricoCreate.as_view(), name='historico-add'),
-    url(r'^historico-edit/(?P<pk>[0-9]+)/$', HistoricoUpdate.as_view(), name='historico-edit'),
-    url(r'^historico-delete/(?P<pk>[0-9]+)/$', HistoricoDelete.as_view(), name='historico-delete'),
+    url(r'^historico/(?P<pk>[0-9]+)/edit/$', HistoricoUpdate.as_view(), name='historico-edit'),
+    url(r'^historico/(?P<id>[0-9]+)/delete/$', historico_delete, name='historico-delete'),
 
     # WORK IN PROGRESS
     url(r'^wip/$', views.work_in_progress, name="wip"),
--- ctb/views.py
+++ ctb/views.py
@@ -1,9 +1,10 @@
+from django.contrib import messages
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib.auth.decorators import login_required
 from django.contrib.messages.views import SuccessMessageMixin
-from django.http import  request
-from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.http import request, HttpResponseRedirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy, reverse
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 
@@ -31,7 +32,7 @@ def ctb_index(request):
 
 class ContaList(SuccessMessageMixin, LoginRequiredMixin, ListView):
     model = Conta
-    template_name = "ctb/conta.html"
+    template_name = "ctb/conta/conta-list.html"
 
     def get_queryset(self):
         descricao = self.request.GET.get('q')
@@ -59,7 +60,7 @@ class ContaList(SuccessMessageMixin, LoginRequiredMixin, ListView):
 class ContaDetalhe(SuccessMessageMixin, LoginRequiredMixin, DetailView):
     model = Conta
     success_message = 'Detalhe registro apresentado com sucesso!'
-    template_name = "ctb/conta_detail.html"
+    template_name = "ctb/conta/conta_detail.html"
 
     def get_success_message(self, cleaned_data):
         return self.success_message % dict(
@@ -72,7 +73,7 @@ class ContaCreate(SuccessMessageMixin, LoginRequiredMixin, CreateView):
     model = Conta
     fields = '__all__'
     success_message = '%(descricao)s criado com sucesso!'
-    template_name = "ctb/conta_form.html"
+    template_name = "ctb/conta/conta_form.html"
 
     def get_success_message(self, cleaned_data):
         return self.success_message % dict(
@@ -85,7 +86,7 @@ class ContaUpdate(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
     model = Conta
     fields = '__all__'
     success_message = '%(descricao)s alterado com sucesso!'
-    template_name = "ctb/conta_form.html"
+    template_name = "ctb/conta/conta_form.html"
 
     def get_success_message(self, cleaned_data):
         return self.success_message % dict(
@@ -94,10 +95,29 @@ class ContaUpdate(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
         )
 
 
-class ContaDelete(SuccessMessageMixin, LoginRequiredMixin, DeleteView):
-    model = Conta
-    template_name = "ctb/confirm_delete.html"
-    success_url = reverse_lazy('ctb:conta-list')
+def conta_delete(request, id=None):
+    obj = get_object_or_404(Conta, id=id)
+    if request.method == 'POST':
+        obj.delete()
+        messages.success(request, 'Registro apagado com sucesso!')
+        return redirect('ctb:conta-list')
+
+    context = {
+        'object': obj
+    }
+
+    return render(request, 'ctb/confirm_delete.html', context)
+
+
+
+
+
+
+
+
+
+
+
 
 """
     WORK IN PROGRESS
@@ -114,29 +134,41 @@ def work_in_progress(request):
 
 class HistoricoList(SuccessMessageMixin, LoginRequiredMixin, ListView):
     model = Historico
-    template_name = 'ctb/historico-index.html'
+    template_name = 'ctb/historico/historico-list.html'
+
+    def get_queryset(self):
+        descricao = self.request.GET.get('q')
+        if descricao:
+            object_list = self.model.objects.filter(descricao__icontains=descricao)
+        else:
+            object_list = self.model.objects.all()
+
+        paginator = Paginator(object_list, 9)  # Show 10 contas per page
+
+        page = self.request.GET.get('page')
+        try:
+            queryset = paginator.page(page)
+        except PageNotAnInteger:
+            # If page is not an integer, deliver first page.
+            queryset = paginator.page(1)
+        except EmptyPage:
+            # If page is out of range (e.g. 9999), deliver last page of results.
+            queryset = paginator.page(paginator.num_pages)
+
+        # return object_list
+        return queryset
 
 
 class HistoricoDetalhe(SuccessMessageMixin, LoginRequiredMixin, DetailView):
     model = Historico
-    template_name = 'ctb/historico-detail.html'
+    template_name = 'ctb/historico/historico_detail.html'
 
 
 class HistoricoCreate(SuccessMessageMixin, LoginRequiredMixin, CreateView):
     model = Historico
     fields = '__all__'
-    success_message = 'Registro criado com sucesso!'
-    template_name = 'ctb/historico_form.html'
-
-    def get_success_message(self, cleaned_data):
-        return self.success_message
-
-
-class HistoricoUpdate(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
-    model = Historico
-    fields = '__all__'
-    success_message = '%(descricao)s alterado com sucesso!'
-    template_name = 'ctb/historico_form.html'
+    success_message = '%(descricao)s criado com sucesso!'
+    template_name = "ctb/historico/historico_form.html"
 
     def get_success_message(self, cleaned_data):
         return self.success_message % dict(
@@ -145,7 +177,29 @@ class HistoricoUpdate(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
         )
 
 
-class HistoricoDelete(SuccessMessageMixin, LoginRequiredMixin, DeleteView):
+class HistoricoUpdate(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
     model = Historico
-    template_name = 'ctb/confirm_delete.html'
-    success_url = reverse_lazy('ctb:historico-list')
+    fields = '__all__'
+    success_message = '%(descricao)s alterado com sucesso!'
+    template_name = 'ctb/historico/historico_form.html'
+
+    def get_success_message(self, cleaned_data):
+        return self.success_message % dict(
+            cleaned_data,
+            calculated_field=self.object.descricao,
+        )
+
+
+def historico_delete(request, id=None):
+    obj = get_object_or_404(Historico, id=id)
+    if request.method == 'POST':
+        obj.delete()
+        messages.success(request, 'Registro apagado com sucesso!')
+        return redirect('ctb:historico-list')
+
+    context = {
+        'object': obj
+    }
+
+    return render(request, 'ctb/confirm_delete.html', context)
+
